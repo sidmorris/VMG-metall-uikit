4844719d954f77ca6742fdeff830df443d4d0ae8
sidmorris <sidmorris@mail.ru>
2017-03-04 21:48:13 +0300

    icon edit

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,10 @@
+$(function() {
 
+	// Custom JS
+    var stickys = UIkit.sticky('#sticky', {
+      //top: 200
+      top: '#stiky-point',
+      animation: 'uk-animation-slide-top'
+    });
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,13 @@ var gulp           = require('gulp'),
 		autoprefixer   = require('gulp-autoprefixer'),
 		cleanCSS       = require('gulp-clean-css'),
 		rename         = require('gulp-rename'),
-		//rigger         = require('gulp-rigger'),
+		concat         = require('gulp-concat'),
+		uglify         = require('gulp-uglify'),
 		pug            = require('gulp-pug'),
 		path           = require('path');
 
 
-gulp.task('serve', ['less', 'pug'], function() {
+gulp.task('serve', ['less', 'pug', 'scripts'], function() {
 
     browserSync.init({
 			server: {
@@ -22,8 +23,8 @@ gulp.task('serve', ['less', 'pug'], function() {
     });
 
     gulp.watch("app/less/**/*.less", ['less']);
-		//gulp.watch('template-html/**/*.html', ['template']);
 		gulp.watch('template/**/*.pug', ['pug']);
+		gulp.watch(['app/js/main.js'], ['scripts']);
     gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
@@ -37,15 +38,9 @@ gulp.task('pug', function buildHTML() {
 			}
 		}))
 		.pipe(gulp.dest('app'))
-		
+
 });
 
-// gulp.task('template', function () {
-//   	return	gulp.src('template-html/**/*.html') //Выберем файлы по нужному пути
-//         .pipe(rigger()) //Прогоним через rigger
-//         .pipe(gulp.dest('app')) //Выплюнем их в папку build
-//         .pipe(browserSync.reload({stream: true})); //И перезагрузим наш сервер для обновлений
-// });
 
 gulp.task('less', function () {
   return gulp.src('app/less/*.less')
@@ -53,12 +48,27 @@ gulp.task('less', function () {
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
+		.pipe(gulp.dest('app/css'))
 		.pipe(rename({suffix: '.min', prefix : ''}))
 		.pipe(autoprefixer(['last 15 versions']))
 		.pipe(cleanCSS())
 		.pipe(sourcemaps.write())
     .pipe(gulp.dest('app/css'))
 		.pipe(browserSync.reload({stream: true}));
+});
+
+// Скрипты проекта
+gulp.task('scripts', function() {
+	return gulp.src([
+			'app/libs/uikit/uikit.min.js',
+			'app/libs/uikit/uikit-icons.min.js'
+		])
+		.pipe(concat('scripts.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('app/js'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
 });
 
 gulp.task('default', ['serve']);
