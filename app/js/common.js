37aac0323ab562b6156931ad024f258f0dd931a9
sidmorris <sidmorris@mail.ru>
2017-04-17 19:28:39 +0300

    deploy new

---
+++ app/js/common.js
@@ -0,0 +1,49 @@
+$(function() {
+
+	$(".owl-carousel.home-promo").owlCarousel({
+		loop: true,
+		margin: 10,
+		navText: ['<span class="uk-transition-fade uk-position-center-left" uk-icon="icon:  chevron-left; ratio: 3"></span>',
+							'<span class="uk-transition-fade uk-position-center-right" uk-icon="icon: chevron-right; ratio: 3"></span>'
+						],
+		navClass: ['uk-transition-toggle', 'uk-transition-toggle'],
+		navContainerClass: 'slidenav',
+		nav: true,
+		responsive:{
+			0:{
+				items:1
+			},
+			640:{
+				items:2
+			},
+			1000:{
+				items:3
+			}
+		}
+	});
+
+	$('.grid').masonry({
+	  // options
+	  itemSelector: '.grid-item'
+	  //columnWidth: 25,
+		//percentPosition: true
+	});
+// =========== magnificPopup ======================================= //
+
+	$('.gallery').each(function() { // the containers for all your galleries
+		$(this).magnificPopup({
+		delegate: 'a', // the selector for gallery item
+		type: 'image',
+		gallery: {
+			enabled:true
+				}
+		});
+	});
+
+	$('.lightbox').magnificPopup({
+		delegate: 'a',
+		type: 'image'
+	  // other options
+	});
+
+});
